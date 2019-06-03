4dd6e563539a2a69fb3e8be6ced7f9d4059439f1
goran-jovicic <gugacns94@gmail.com>
2019-06-03 21:08:45 +0200

    zadatak 4

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -9,7 +9,7 @@ class RegisterController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('guest');
+        $this->middleware('guest', ['except' => 'destroy']);
     }
 
     public function create()
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,21 +4,15 @@
                 <a class="nav-link" href="/teams">
                     <h2 style="color: white;">Teams</h2>
                 </a>
-                @if(!Auth::check())
                 <a class="nav-link" href="/register">
                     <h2 style="color: white;">Register</h2>
                 </a>
-                @endif
-                @if(!Auth::check())
                 <a class="nav-link" href="/login">
                     <h2 style="color: white;">Login</h2>
                 </a>
-                @endif
-                @if(Auth::check())
                 <a class="nav-link" href="/logout">
                     <h2 style="color: white;">Logout</h2>
                 </a>
-                @endif
             </div>
         </div>
         <hr>
